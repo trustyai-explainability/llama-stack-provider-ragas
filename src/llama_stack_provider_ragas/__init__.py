69ae9c2587b7275a929045e1b244236dd36c45a4
Diego Maniloff <diego.maniloff@gmail.com>
2025-07-27 07:39:07 -0400

    inline & placeholder for remote.

--- src/llama_stack_provider_ragas/__init__.py
+++ src/llama_stack_provider_ragas/__init__.py
@@ -3,17 +3,24 @@ from typing import Any, Dict
 from llama_stack.distribution.datatypes import Api
 
 from .config import RagasEvalProviderConfig
-from .ragas_eval import RagasEvaluator
+from .ragas_eval import RagasEvaluatorInline, RagasEvaluatorRemote
 
 
 async def get_adapter_impl(
     config: RagasEvalProviderConfig,
     deps: Dict[Api, Any],
-) -> RagasEvaluator:
-    return RagasEvaluator(config, deps[Api.datasetio], deps[Api.inference])
+) -> RagasEvaluatorRemote:
+    return RagasEvaluatorRemote(config, deps[Api.datasetio], deps[Api.inference])
+
+
+async def get_provider_impl(
+    config: RagasEvalProviderConfig,
+    deps: Dict[Api, Any],
+) -> RagasEvaluatorInline:
+    return RagasEvaluatorInline(config, deps[Api.datasetio], deps[Api.inference])
 
 
 __all__ = [
     "get_adapter_impl",
-    "RagasEvaluator",
+    "get_provider_impl",
 ]
