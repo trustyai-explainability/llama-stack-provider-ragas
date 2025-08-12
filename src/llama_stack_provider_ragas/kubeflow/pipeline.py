b4c54e850c1e324053368613ce274b67e3024db4
Diego Maniloff <diego.maniloff@gmail.com>
2025-08-12 14:19:05 -0400

    write to s3: secret as env 2.0.

--- src/llama_stack_provider_ragas/kubeflow/pipeline.py
+++ src/llama_stack_provider_ragas/kubeflow/pipeline.py
@@ -34,6 +34,15 @@ def ragas_evaluation_pipeline(
         metrics=metrics,
         llama_stack_base_url=llama_stack_base_url,
     )
+    kubernetes.use_secret_as_env(
+        ragas_result,
+        secret_name="aws-credentials",
+        secret_key_to_env={
+            "AWS_ACCESS_KEY_ID": "AWS_ACCESS_KEY_ID",
+            "AWS_SECRET_ACCESS_KEY": "AWS_SECRET_ACCESS_KEY",
+            "AWS_DEFAULT_REGION": "AWS_DEFAULT_REGION",
+        },
+    )
     # TODO: need to store the ragas_result.uri to later retrieve the results
 
 
