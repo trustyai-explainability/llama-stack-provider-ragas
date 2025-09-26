a9d1b28bb8ad3f664d32d907cacff67292766c01
Diego Maniloff <diego.maniloff@gmail.com>
2025-09-25 20:10:47 -0400

    cosmit: format.

--- src/llama_stack_provider_ragas/remote/provider.py
+++ src/llama_stack_provider_ragas/remote/provider.py
@@ -12,7 +12,13 @@ def get_provider_spec() -> ProviderSpec:
         adapter=AdapterSpec(
             adapter_type="trustyai_ragas",
             module="llama_stack_provider_ragas.remote",
-            pip_packages=["ragas==0.3.0", "kfp>=2.5.0", "kfp-kubernetes>=2.0.0", "s3fs>=2024.12.0", "kubernetes>=30.0.0"],
+            pip_packages=[
+                "ragas==0.3.0",
+                "kfp>=2.5.0",
+                "kfp-kubernetes>=2.0.0",
+                "s3fs>=2024.12.0",
+                "kubernetes>=30.0.0",
+            ],
             config_class="llama_stack_provider_ragas.config.RagasProviderRemoteConfig",
         ),
         api_dependencies=[
