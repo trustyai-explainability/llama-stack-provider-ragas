c76d53c2a8a4e736af4080500f39eadba3af5df8
Diego Maniloff <diego.maniloff@gmail.com>
2025-09-25 20:09:11 -0400

    match provider versions to pyproject.toml

--- src/llama_stack_provider_ragas/inline/provider.py
+++ src/llama_stack_provider_ragas/inline/provider.py
@@ -5,7 +5,7 @@ def get_provider_spec() -> ProviderSpec:
     return InlineProviderSpec(
         api=Api.eval,
         provider_type="inline::trustyai_ragas",
-        pip_packages=["ragas"],
+        pip_packages=["ragas==0.3.0"],
         config_class="llama_stack_provider_ragas.config.RagasProviderInlineConfig",
         module="llama_stack_provider_ragas.inline",
         api_dependencies=[
